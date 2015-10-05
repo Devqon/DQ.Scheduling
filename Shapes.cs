5dbb5ff68e83260f48a3f91ed16554c05c4203c4
Devqon <s.geerts@live.nl>
2015-10-05 14:37:48 +0200

    Bugfix only active provider shapes

--- Shapes.cs
+++ Shapes.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using DQ.Scheduling.CalendarProviders;
 using Orchard.DisplayManagement.Descriptors;
 using Orchard.Environment.Extensions;
 
@@ -5,11 +8,21 @@ namespace DQ.Scheduling {
     [OrchardFeature("DQ.CalendarWidget")]
     public class Shapes : IShapeTableProvider {
 
+        private readonly IEnumerable<ICalendarProvider> _providers;
+        public Shapes(IEnumerable<ICalendarProvider> providers) {
+            _providers = providers;
+        }
+
         public void Discover(ShapeTableBuilder builder) {
             builder.Describe("Parts_CalendarWidget")
                 .OnDisplaying(displaying => {
                     string plugin = displaying.Shape.Plugin;
-                    displaying.ShapeMetadata.Alternates.Add("Parts_CalendarWidget__" + plugin);
+                    var provider = _providers.FirstOrDefault(p => p.Name == plugin);
+
+                    // Only if the provider is enabled
+                    if (provider != null) {
+                        displaying.ShapeMetadata.Alternates.Add("Parts_CalendarWidget__" + plugin);
+                    }
                 });
         }
     }
