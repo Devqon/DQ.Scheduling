e9327f0a75c1592e2abe13734674525304d06a66
Jeff Olmstead <jeff.olmstead@endlessmountainsolutions.com>
2015-10-06 08:58:33 -0400

    Utilized UrlHelper via dependency injection and IContentManager instead of IOrchardServices as it has less overhead.

--- CalendarProviders/FullCalendarProvider.cs
+++ CalendarProviders/FullCalendarProvider.cs
@@ -1,42 +1,39 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using DQ.Scheduling.Models;
+﻿using DQ.Scheduling.Models;
 using DQ.Scheduling.ViewModels;
-using Orchard;
 using Orchard.ContentManagement;
 using Orchard.Environment.Extensions;
 using Orchard.Mvc.Html;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
-namespace DQ.Scheduling.CalendarProviders
-{
+namespace DQ.Scheduling.CalendarProviders {
     [OrchardFeature("DQ.FullCalendar")]
-    public class FullCalendarProvider : ICalendarProvider
-    {
-        private readonly IOrchardServices _orchardServices;
-        public FullCalendarProvider(IOrchardServices orchardServices)
-        {
-            _orchardServices = orchardServices;
+    public class FullCalendarProvider : ICalendarProvider {
+        private readonly IContentManager _contentManager;
+        private readonly UrlHelper _urlHelper;
+
+        public FullCalendarProvider(IContentManager contentManager
+            , UrlHelper urlHelper) {
+
+            _contentManager = contentManager;
+            _urlHelper = urlHelper;
         }
 
         public string Name { get { return "Fullcalendar"; } }
 
         public IEnumerable<SerializedEvent> SerializeEvents(IEnumerable<IContent> events) {
             var viewModels = new List<FullCalendarEventViewModel>();
-
-            var currentContext = _orchardServices.WorkContext;
-            var urlHelper = new UrlHelper(currentContext.HttpContext.Request.RequestContext);
-
+            
             events.ToList().ForEach(ev => {
                 var eventPart = ev.As<EventDefinitionPart>();
 
-                var viewModel = new FullCalendarEventViewModel
-                {
+                var viewModel = new FullCalendarEventViewModel {
                     Id = ev.Id,
-                    Title = _orchardServices.ContentManager.GetItemMetadata(ev).DisplayText,
+                    Title = _contentManager.GetItemMetadata(ev).DisplayText,
                     Start = eventPart.StartDateTime.GetValueOrDefault(),
                     End = eventPart.EndDateTime.GetValueOrDefault(),
-                    Url = urlHelper.ItemDisplayUrl(ev),
+                    Url = _urlHelper.ItemDisplayUrl(ev),
                     AllDay = eventPart.IsAllDay
                 };
 
