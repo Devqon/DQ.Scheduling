ce89db3eb64431f950029ca46d80980cda8a30c1
Devqon <s.geerts@live.nl>
2015-10-13 14:02:58 +0200

    Admin menu item

---
+++ AdminMenu.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace DQ.Scheduling {
+    public class AdminMenu : INavigationProvider {
+        public Localizer T { get; set; }
+
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.AddImageSet("scheduledEvents")
+                .Add(T("Scheduled Events"), "4",
+                    menu => menu.Add(T("List"), "0", item => item.Action("Index", "SchedulingAdmin", new { area = "DQ.Scheduling" })));
+        }
+    }
+}
